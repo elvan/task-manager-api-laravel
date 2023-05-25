be0052a0f3da27284991cca8eb3b505471c4d065
Elvan Hidayat <elvanhd@gmail.com>
2023-05-25 20:23:30 +0700

    create a get all tasks route

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    public function index(Request $request)
+    {
+        return response()->json(Task::all());
+    }
+}
