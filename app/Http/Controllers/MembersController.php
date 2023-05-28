3986bd908a6787e5a6ff149fe50fe5e4121d5a10
Elvan Hidayat <elvanhd@gmail.com>
2023-05-28 14:23:51 +0700

    adding a nested resource

---
+++ app/Http/Controllers/MembersController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\UserCollection;
+use App\Models\Project;
+use Illuminate\Http\Request;
+
+class MembersController extends Controller
+{
+    public function index(Request $request, Project $project)
+    {
+        $members = $project->members;
+
+        return new UserCollection($members);
+    }
+}
